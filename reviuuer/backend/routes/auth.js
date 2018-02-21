a4d55b9d80d2ac2cc8d1204239bd2cef1a641d6c
Linus Frosteryd <linus.frosteryd@gmail.com>
2018-02-21 14:12:19 +0100

    Working webtoken, still stupid with access flag. But now we can check each user! FETT

--- reviuuer/backend/routes/auth.js
+++ reviuuer/backend/routes/auth.js
@@ -7,12 +7,28 @@ var mysqlConf = require('../config.js').mysql_pool;
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+var secretJWT = '@Q4&SuqQegjJwUkQy@rMNW2@hetjsZwKgs&@guV^MvG^$tbrvN34GKn^D#Mz5^cmrHzjWbvF$YqQzy6Mr'
+var jwt  = require('jsonwebtoken');
+// var secret = process.env.JWT_SECRET // use this later and set enviormental variable when we run node
+
 session = require('express-session');
 app.use(session({
     secret: '2C44-4D44-WppQ38S',
     resave: true,
     saveUninitialized: true
 }));
+
+function generateToken(email, admin, access){
+  var u = {
+   email: email,
+   admin: admin,//user.admin, // TODO add so we can manage admin in DB
+   access: access, //remove this and add a user id for each user THIS is no good
+  };
+  return token = jwt.sign(u, secretJWT,{
+     expiresIn: 60 * 60 * 24 * 30 // expires in 30 days
+  });
+}
+
 
 const authenticateUser = (email, myPlaintextPassword, cb) => {
   mysqlConf.getConnection(function (err, connection) {
@@ -24,15 +40,15 @@ const authenticateUser = (email, myPlaintextPassword, cb) => {
       connection.release();
 
       if(error) console.error(error);
-  
+
       if(results.length){
         var bcryptAuth = bcrypt.compareSync(myPlaintextPassword, results[0].password);
         if(bcryptAuth) {
           console.log('Authentication accepted');
           cb(error, true);
           return;
-        } 
-      } 
+        }
+      }
       console.log('Authentication declined');
       cb(error, false);
       return;
@@ -41,20 +57,21 @@ const authenticateUser = (email, myPlaintextPassword, cb) => {
 }
 
 //TODO validate cookie here
-const authenticateCookie = (cookie, cb) => { 
-    console.log(cookie);
-    if (cookie) {
-      if (cookie === 'true') {
-        console.log("True callback i funktion")
-        cb( true ); 
-        return;
-      }
+const authenticateCookie = (cookie, cb) => {
+    jwt.verify(cookie, secretJWT, function(err, user) {
+    if (err) throw err;
+    if (user.access === true){
+      console.log("True callback i funktion");
+      console.log(user);
+      cb( true );
+      return;
     }
 
-    console.log("False callback i funktion")
+    console.log("False callback i funktion");
     cb( false );
     return;
 
+    });
 }
 
 
@@ -67,14 +84,17 @@ router.get('/', function(req, res) {
     if(email !== undefined && password !== undefined) { // loggar in
       authenticateUser(email, password, (error, access) => {
         if (access) {
-            console.log('Authenticated')
-            res.json({access: true, 
-                      token: true}); //TODO GENERATE TOKEN FOR COOKIE
+            console.log('Authenticated');
+            var admin = false;
+            var access = true;
+            var tokenToSet = generateToken(email, admin, access);
+            res.json({access: true,
+                      token: tokenToSet}); //TODO GENERATE TOKEN FOR COOKIE
         } else {
             console.log('Not authenticated')
             res.json({access: false});
-        } 
-      }); 
+        }
+      });
     } else if (cookie !== undefined) { // redan inloggad
       authenticateCookie(cookie, (accessCookie) => {
         if(accessCookie === true){
@@ -83,7 +103,7 @@ router.get('/', function(req, res) {
         } else {
             console.log('Not authenticated')
             res.json({accessCookie: false});
-        } 
+        }
       });
     } else {
       console.log('Authentication failed')
