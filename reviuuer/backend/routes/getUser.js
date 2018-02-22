8e855da8e1841daadcd36c0fd125b83463bcd2a1
fatrique <lar.patrik@gmail.com>
2018-02-22 13:43:43 +0100

    Added backend support for fetching user info by supplying email

---
+++ reviuuer/backend/routes/getUser.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var mysqlConf = require('../config.js').mysql_pool;
+
+const fetchUser = (cb) => {
+  mysqlConf.getConnection(function (err, connection) {
+    connection.query({
+      sql: 'SELECT * FROM user WHERE email = ?',
+      timeout: 40000, // 40s
+      values: [email]
+    }, function (error, results, fields) {
+      connection.release();
+
+      if(error) console.error(error);
+      
+      cb(error, results);
+      console.log(results);
+      return;
+    });
+  });
+}
+
+/* GET teachers. */
+router.get('/', function(req, res) {
+  var email = req.param('email');
+
+  fetchUser(email, (error, user) => {
+      res.json(user);
+    })
+});
+
+module.exports = router;
