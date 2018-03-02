b57d9f1f02e9f098a970efe35ac66ccaceea3612
fatrique <lar.patrik@gmail.com>
2018-03-02 20:36:04 +0100

    Added functionality to rate the attributes from 1 to 5 by clicking

--- reviuuer/src/components/PortalAddReview.js
+++ reviuuer/src/components/PortalAddReview.js
@@ -33,14 +33,10 @@ class PortalAddReview extends React.Component {
   }
 
   state = {
-    review: undefined,
-    comments: [],
-    addComment: false,
-    newComment: "",
-
     course_options: [],
     user_id: '1',
     course_id: null,
+    teacher_name: '',
     quality: '',
     difficulty: '',
     can_recommend: '',
@@ -54,7 +50,6 @@ class PortalAddReview extends React.Component {
 
   handleChange = (selectedOption) => {
     this.setState({ selectedOption });
-    console.log(`Selected: ${selectedOption.label}`);
   }
 
   handleSubmit(event) {
@@ -129,7 +124,11 @@ class PortalAddReview extends React.Component {
     document.body.classList.add('portal'); //adding the correct background by setting the class of the body
   }
 
-  printGradedScale(rating) {
+  handleGradedScaleClick(rating, cb) {
+    cb(rating);
+  }
+
+  printGradedScale(rating, cb) {
     let filled = ["", "", "", "", ""];
 
     for (let i = 0; i < rating; i++) {
@@ -138,11 +137,11 @@ class PortalAddReview extends React.Component {
 
     return(
       <div className="gradedScaleBody">
-        <div className={"leftScaleItem " + filled[0]}/>
-        <div className={"middleScaleItem " + filled[1]}/>
-        <div className={"middleScaleItem " + filled[2]}/>
-        <div className={"middleScaleItem " + filled[3]}/>
-        <div className={"rightScaleItem " + filled[4]}/>
+        <div className={"leftScaleItem " + filled[0]} onClick={() => this.handleGradedScaleClick(1, cb)}/>
+        <div className={"middleScaleItem " + filled[1]} onClick={() => this.handleGradedScaleClick(2, cb)}/>
+        <div className={"middleScaleItem " + filled[2]} onClick={() => this.handleGradedScaleClick(3, cb)}/>
+        <div className={"middleScaleItem " + filled[3]} onClick={() => this.handleGradedScaleClick(4, cb)}/>
+        <div className={"rightScaleItem " + filled[4]} onClick={() => this.handleGradedScaleClick(5, cb)}/>
       </div>
     );
   }
@@ -187,55 +186,64 @@ class PortalAddReview extends React.Component {
 
 
   render() {
-    const { review } = this.state;
-
-    console.log("Selected course_id = " + this.state.course_id);
+    console.log(this.state);
 
     return (
       <div className="portal">
         <div className='header'>
           <img onClick={this.context.router.history.goBack} src={backArrow} className="backArrow" />
         </div>
-        {/* <form className="form" onSubmit={this.handleSubmit}>
-          <input className="login" placeholder="course_id" value={this.state.course_id} onChange={(event) => this.setState({course_id: event.target.value})} /><br/>
-          <input className="login" placeholder="quality" value={this.state.quality} onChange={(event) => this.setState({quality: event.target.value})} /><br/>
-          <input className="login" placeholder="difficulty" value={this.state.difficulty} onChange={(event) => this.setState({difficulty: event.target.value})} /><br/>
-          <input className="login" placeholder="can_recommend" value={this.state.can_recommend} onChange={(event) => this.setState({can_recommend: event.target.value})} /><br/>
-          <input className="login" placeholder="worth_credits" value={this.state.worth_credits} onChange={(event) => this.setState({worth_credits: event.target.value})} /><br/>
-          <input className="login" placeholder="books_req" value={this.state.books_req} onChange={(event) => this.setState({books_req: event.target.value})} /><br/>
-          <input className="login" placeholder="percentage_mand" value={this.state.percentage_mand} onChange={(event) => this.setState({percentage_mand: event.target.value})} /><br/>
-          <input className="login" placeholder="exam" value={this.state.exam} onChange={(event) => this.setState({exam: event.target.value})} /><br/>
-          <input className="login" placeholder="course_review" value={this.state.course_review} onChange={(event) => this.setState({course_review: event.target.value})} /><br/>
-          <input className="login" placeholder="teacher_review" value={this.state.teacher_review} onChange={(event) => this.setState({teacher_review: event.target.value})} /><br/>
+        <form className="new-review" onSubmit={this.handleSubmit}>
+            {/* <input className="login" placeholder="course_id" value={this.state.course_id} onChange={(event) => this.setState({course_id: event.target.value})} /><br/>
+            <input className="login" placeholder="quality" value={this.state.quality} onChange={(event) => this.setState({quality: event.target.value})} /><br/>
+            <input className="login" placeholder="difficulty" value={this.state.difficulty} onChange={(event) => this.setState({difficulty: event.target.value})} /><br/>
+            <input className="login" placeholder="can_recommend" value={this.state.can_recommend} onChange={(event) => this.setState({can_recommend: event.target.value})} /><br/>
+            <input className="login" placeholder="worth_credits" value={this.state.worth_credits} onChange={(event) => this.setState({worth_credits: event.target.value})} /><br/>
+            <input className="login" placeholder="books_req" value={this.state.books_req} onChange={(event) => this.setState({books_req: event.target.value})} /><br/>
+            <input className="login" placeholder="percentage_mand" value={this.state.percentage_mand} onChange={(event) => this.setState({percentage_mand: event.target.value})} /><br/>
+            <input className="login" placeholder="exam" value={this.state.exam} onChange={(event) => this.setState({exam: event.target.value})} /><br/>
+            <input className="login" placeholder="course_review" value={this.state.course_review} onChange={(event) => this.setState({course_review: event.target.value})} /><br/>
+            <input className="login" placeholder="teacher_review" value={this.state.teacher_review} onChange={(event) => this.setState({teacher_review: event.target.value})} /><br/> */}
 
-          <input className="submit" type="submit" value="SUBMIT" />
+
+            <h2 className="select-text">Select course</h2>
+            <SelectCourse value={this.state.course_id} onChangeCB={this.cbChangeCourse.bind(this)} options={this.state.course_options} />
+            <h2 className="select-text">Select teacher</h2>
+            <input className="teacher-name" placeholder="Write teacher(s) name(s) here..." value={this.state.teacher_name} onChange={(event) => this.setState({teacher_name: event.target.value})} />
+
+            <hr className="review"/>
+            <h2 className="attributesStyle">Quality<br/>
+            {/* {this.printGradedScale(0, (rating) => { console.log(rating); } )} */}
+            {this.printGradedScale(this.state.quality, (rating) => { this.setState({quality: rating}); })}
+            </h2>
+            <h2 className="attributesStyle">Difficulty<br/>
+            {this.printGradedScale(this.state.difficulty, (rating) => { this.setState({difficulty: rating}); })}
+            </h2>
+            <h2 className="attributesStyle">Worth credits<br/>
+            {this.printGradedScale(this.state.worth_credits, (rating) => { this.setState({worth_credits: rating}); })}
+            </h2>
+            <h2 className="attributesStyle">Percentage mandatory<br/>
+            {this.printGradedScale(this.state.percentage_mand, (rating) => { this.setState({percentage_mand: rating}); })}
+            </h2>
+            <h2 className="attributesStyle">Books required: </h2>
+            {0 ? this.printRadio(true) : this.printRadio(false)}
+            <h2 className="attributesStyle">Has exam: </h2>
+            {0 ? this.printRadio(true) : this.printRadio(false)}
+            <h2 className="attributesStyle">Can reccommend: </h2>
+            {0 ?
+            <img onClick={this.context.router.history.goBack} src={thumbGreen} className="thumb" /> :
+            <img onClick={this.context.router.history.goBack} src={thumbRed} className="thumb" />}
+            <hr className="review"/>
+            <h2 className="attributesStyle">Course review: </h2>
+            <textarea cols="40" rows="5" className="text-review" placeholder="Write course review here..." value={this.state.course_review} onChange={(event) => this.setState({course_review: event.target.value})} />
+
+            <h2 className="attributesStyle">Teacher review: </h2>
+            <textarea cols="40" rows="5" className="text-review" placeholder="Write teacher review here..." value={this.state.teacher_review} onChange={(event) => this.setState({teacher_review: event.target.value})} />
+
+            <hr className="review"/>
+
+            <input className="submit" type="submit" value="SUBMIT" />
         </form>
-         */}
-        <div className="reviewContainer">
-          <h2 className="select-text">Select course</h2>
-          <SelectCourse value={this.state.course_id} onChangeCB={this.cbChangeCourse.bind(this)} options={this.state.course_options} />
-          <h2 className="select-text">Select teacher</h2>
-
-          <hr className="review"/>
-          <h2 className="attributesStyle">Quality<br/> {this.printGradedScale(0)}</h2>
-          <h2 className="attributesStyle">Difficulty<br/> {this.printGradedScale(0)}</h2>
-          <h2 className="attributesStyle">Worth credits<br/> {this.printGradedScale(0)}</h2>
-          <h2 className="attributesStyle">Percentage mandatory<br/> {this.printGradedScale(0)}</h2>
-          <h2 className="attributesStyle">Books required: </h2>
-          {0 ? this.printRadio(true) : this.printRadio(false)}
-          <h2 className="attributesStyle">Has exam: </h2>
-          {0 ? this.printRadio(true) : this.printRadio(false)}
-          <h2 className="attributesStyle">Can reccommend: </h2>
-          {0 ?
-          <img onClick={this.context.router.history.goBack} src={thumbGreen} className="thumb" /> :
-          <img onClick={this.context.router.history.goBack} src={thumbRed} className="thumb" />}
-          <hr className="review"/>
-          <h2 className="attributesStyle">Course review: </h2>
-          <p className="reviewText">{0}</p>
-          <h2 className="attributesStyle">Teacher review: </h2>
-          <p className="reviewText">{0}</p>
-          <hr className="review"/>
-        </div>
 
         <Footer/>
       </div>
