bcca9bb5b8d618f69bdc3b67fcda7c900c6e152e
fatrique <lar.patrik@gmail.com>
2018-03-10 00:20:40 +0100

    If login fields are left empty you log in as admin. If not empty the content of the fields are read

--- reviuuer/src/components/LoginPage.js
+++ reviuuer/src/components/LoginPage.js
@@ -35,10 +35,9 @@ class Login extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    // var email = this.state.email
-    // var pwd = this.state.password
-    var email = 'admin@reviuuer.se' // TODO remove this later
-    var pwd = 'admin'  // TODO remove this later
+    var email = this.state.email === '' ? this.state.email: 'admin@reviuuer.se' // TODO remove this later
+    var pwd = this.state.password === '' ? this.state.password : 'admin'  // TODO remove this later
+
     var fetchURL = `/api/auth?email=${email}&password=${pwd}`;
     fetch( fetchURL )
       .then(
